624df6f6f48a290e9e638a6d387b99232d2f5537
nurzainpradana <pradananurzain@gmail.com>
2020-11-17 09:31:55 +0700

    update variable to mutableLiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,18 +1,21 @@
 package com.example.android.guesstheword.screens.game
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     // The current word
-    var word = ""
+    var word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    var score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
+        word.value = ""
+        score.value = 0
         resetList()
         nextWord()
     }
@@ -53,10 +56,19 @@ class GameViewModel : ViewModel() {
     fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
 
+    fun onSkip() {
+        score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score.value = (score.value)?.plus(1)
+        nextWord()
+    }
 
 
 
